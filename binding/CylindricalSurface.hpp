7227009b09301b7827070a9dd8763e2152945d49
Thomas Sokolowski <sokolowski@bionet3.amolf.nl>
2012-11-27 11:07:34 +0100

    Use peer getter/setter formalism to bind private rate properties of CylindricalSurface

--- binding/CylindricalSurface.hpp
+++ binding/CylindricalSurface.hpp
@@ -2,6 +2,8 @@
 #define BINDING_CYLINDRICAL_SURFACE_HPP
 
 #include <boost/python.hpp>
+#include "peer/utils.hpp"
+#include "Defs.hpp"
 
 namespace binding {
 
@@ -19,8 +21,26 @@ inline boost::python::objects::class_base register_cylindrical_surface_class(cha
                        typename impl_type::structure_type_id_type,
                        typename impl_type::structure_id_type,
                        typename impl_type::shape_type>())
+        .def(init<typename impl_type::structure_name_type,
+                       typename impl_type::structure_type_id_type,
+                       typename impl_type::structure_id_type,
+                       typename impl_type::shape_type, Real, Real>())
         .add_property("shape",
             make_function((typename impl_type::shape_type const&(impl_type::*)()const)&impl_type::shape, return_value_policy<reference_existing_object>()))
+        .add_property("growth_rate",
+            make_function(
+                &peer::util::reference_accessor_wrapper<
+                    impl_type,
+                    Real,
+                    &impl_type::growth_rate,
+                    &impl_type::growth_rate>::get,
+                return_value_policy<return_by_value>()),
+            make_function(
+                &peer::util::reference_accessor_wrapper<
+                    impl_type,
+                    Real,
+                    &impl_type::growth_rate,
+                    &impl_type::growth_rate>::set))
         ;
 }
 
