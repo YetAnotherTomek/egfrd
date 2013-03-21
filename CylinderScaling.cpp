55ba45937ed462e2edbfa84e23d0fdb279dde7cc
Thomas Sokolowski <sokolowski@bionet3.amolf.nl>
2013-03-21 19:27:41 +0100

    Again missing typename

--- CylinderScaling.cpp
+++ CylinderScaling.cpp
@@ -25,7 +25,7 @@
 
 // TESTING
 template <typename Ttraits_>
-Ttraits_::position_type
+typename Ttraits_::position_type
 get_dr_dzright_dzleft_to_CylindricalShape_helper( typename Ttraits_::position_type      const& pos,
                                                   typename Ttraits_::length_type        const& l1,
                                                   typename Ttraits_::length_type        const& l2   )
